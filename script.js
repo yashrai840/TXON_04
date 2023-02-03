49a2ee8cc0d894b55cc099c32ac5410ccf4addc2
Yash rai <99031345+yashrai840@users.noreply.github.com>
2023-02-03 21:42:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+var typed=new Typed(".typing",{
+    strings:["","Frontend Developer","Coder","Programmer","Problem Solver","Keen Learner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+                                    
